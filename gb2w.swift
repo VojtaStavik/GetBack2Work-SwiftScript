d653fc61c0a8a1dbf102d98a5b3c3b4e46193869
Vojta Stavik <vojta@strv.com>
2015-08-03 11:24:19 +0200

    Initial commit

---
+++ gb2w.swift
@@ -0,0 +1,48 @@
+#!/usr/bin/env xcrun -sdk macosx swift -F /Users/vojtastavik/Documents/GB2W/Rome
+
+import Foundation
+import Alamofire
+
+
+class MainProcess {
+    var shouldExit = false
+    
+    func start () {
+
+        if Process.arguments.count >= 2 {
+
+            Alamofire
+                .request(.POST, "http://getback2work.azurewebsites.net/send", parameters: ["username": Process.arguments[1]])
+                .responseString { _, _, data, _ in
+                    
+                    println(data)
+                    self.shouldExit = true
+            }
+        }
+    }
+}
+
+
+
+
+var runLoop : NSRunLoop
+var process : MainProcess
+
+autoreleasepool {
+    runLoop = NSRunLoop.currentRunLoop()
+    process = MainProcess()
+    
+    process.start()
+    
+    while (!process.shouldExit && (runLoop.runMode(NSDefaultRunLoopMode, beforeDate: NSDate(timeIntervalSinceNow: 2)))) {
+        // do nothing
+    }
+}
+
+
+
+
+
+
+
+
